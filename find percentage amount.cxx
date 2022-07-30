8a127c6881d82bd7ec5dd16aafc019d894f0940c
JASEEL P <100408465+Jazeye@users.noreply.github.com>
2022-07-30 16:07:15 +0530

    Add files via upload

---
+++ find percentage amount.cxx
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ float main()
+{
+printf("Enter the percentage  :");
+float a,b,pe;
+scanf("%f",&a);
+printf("Enter total amount :");
+scanf("%f",&b);
+pe=a/100*b;
+printf(" Your percentage amount  is : %f",pe);
+}
